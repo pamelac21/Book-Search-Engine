dff37dd81660e9aaecc7179aa49e37eb8d537fcc
PamelaCleveland <pamelac021@gmail.com>
2022-01-06 16:55:04 -0500

    added heroku port

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/BookSearchEngine', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/BookSearchEngine', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
